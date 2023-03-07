c971d8b3cc99f42e92749ce2400d59725cac4a88
Lucielee-mendes <lucielee28@gmail.com>
2023-03-07 13:54:45 -0300

    entrega

---
+++ 0.1.0/exercicio1.js
@@ -0,0 +1,24 @@
+let valor=0;
+let desc = false;
+let cartao = false;
+const calcValue =(valor, desc, cartao)=>{
+
+    if(desc && cartao){
+        //desconto 15%
+        let desconto = valor*0.15;
+        let calc = valor - desconto;
+        return calc;
+    }else if(desc || cartao){
+        //desconto 10%
+        let desconto = valor*0.10;
+        let calc = valor - desconto;
+        return calc;
+    }
+    else{
+        return valor;
+    }
+
+}
+
+
+console.log("O VALOR SERA DE??", calcValue(100, true, true))
