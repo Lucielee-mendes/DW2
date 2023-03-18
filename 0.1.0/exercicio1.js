a753b4ba0ef8f4c66093e56f0b3f7ad81018f62c
Lucielee-mendes <lucielee28@gmail.com>
2023-03-18 00:37:27 -0300

    entregaa

--- 0.1.0/exercicio1.js
+++ 0.1.0/exercicio1.js
@@ -1,24 +1,21 @@
-let valor=0;
-let desc = false;
-let cartao = false;
-const calcValue =(valor, desc, cartao)=>{
 
-    if(desc && cartao){
+function calcValue (valor, conv, cartao){
+
+    if(conv && cartao){
         //desconto 15%
         let desconto = valor*0.15;
         let calc = valor - desconto;
         return calc;
-    }else if(desc || cartao){
+    }else if(conv || cartao){
         //desconto 10%
         let desconto = valor*0.10;
         let calc = valor - desconto;
         return calc;
     }
-    else{
         return valor;
-    }
+    
 
 }
 
 
-console.log("O VALOR SERA DE??", calcValue(100, true, true));
+console.log("O VALOR SERA DE??", calcValue(100, true, false));
