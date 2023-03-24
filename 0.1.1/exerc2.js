ba4b7d47514ca3290724cac9b994fb323489273b
Lucielee-mendes <112039537+Lucielee-mendes@users.noreply.github.com>
2023-03-24 20:41:33 -0300

    Update exerc2.js

--- 0.1.1/exerc2.js
+++ 0.1.1/exerc2.js
@@ -1,26 +1,35 @@
 const anos = ['2012', '2010', '2020', '2022' , '2035', '2045'];
 
-const anoPassado  = '2022';
 function  menoresAnos (anoPassado){
     let list=[]
     for (let i = 0; i < anos.length; i++){
-        if(anos[i]<anoPassado){
+        if(Number(anos[i]) < Number(anoPassado)){
             list.push(anos[i])
         }
     }
     return list
 }
 
-console.log(menoresAnos('2022'));
+console.log(menoresAnos(2022));
 
 function maiorOuIgualAnos (anoPassado){
     let list=[]
     for (let i = 0; i < anos.length; i++){
-        if(anos[i]>=anoPassado){
+        if(Number(anos[i]) >= Number(anoPassado)){
             list.push(anos[i])
         }
     }
     return list.length
 }
-console.log(maiorOuIgualAnos('2022'));
+console.log(maiorOuIgualAnos(2022));
 
+function anosDecimos (divisor = 10){
+    let list=[]
+    for (let i = 0; i < anos.length; i++){
+        if(Number(anos[i]) % +divisor === 0){
+            list.push(anos[i])
+        }
+    }
+    return list.length
+}
+console.log(anosDecimos());
