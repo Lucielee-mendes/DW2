6cb997795ebcefb72e35f83a74e6700e382b6cfc
Lucielee-mendes <lucielee28@gmail.com>
2023-03-24 22:11:03 -0300

    entrega

--- 0.1.2/exer3.js
+++ 0.1.2/exer3.js
@@ -1,24 +1,30 @@
+const descontoTotal = 100;
+function limiteDesconto (desc){
+    if(des > descontoTotal){
+        desc = descontoTotal;
+    }
+    return desc;
+}
 
 function calcValue (valor, desc, cartao){
 
-    if(valor<=100){
+    
         if(desc && cartao){
             //desconto 15%
             let desconto = valor*0.15
-            let calc = valor - desconto
+            let calc = (valor - limiteDesconto(desconto))
             return calc;
         }else if(desc || cartao){
             //desconto 10%
             let desconto = valor*0.10
-            let calc = valor - desconto
+            let calc = (valor - limiteDesconto(desconto))
             return calc;
         }
         else{
             return valor;
         }
-    }else{
-        return valor
-    }
+
+    
 }
 
 console.log("O VALOR SERA DE: ", calcValue(101, true, true))
